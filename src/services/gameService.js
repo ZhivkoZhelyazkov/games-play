7a9a60989392a3fdaa3caa49456af201fd928a28
ZhivkoZhelyazkov <zkzhelyazkov@gmail.com>
2021-11-15 13:24:53 +0200

    Add Firebase hosting

--- src/services/gameService.js
+++ src/services/gameService.js
@@ -3,8 +3,14 @@ const baseUrl = 'http://localhost:3030/data';
 export function getAll() {
     return fetch(`${baseUrl}/games?sortBy=_createdOn%20desc`)
         .then(response => response.json());
-}
+};
 
 
 export const getOne = (id) => fetch(`${baseUrl}/games/${id}`)
     .then(response => response.json());
+
+
+export const getLatest = () => {
+    return fetch(`${baseUrl}/games?sortBy=_createdOn%20desc&distinct=category`)
+        .then(response => response.json());
+};
